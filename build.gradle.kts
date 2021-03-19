0a82f6bdd10c1b3146670845f9896312ae3256be
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-03-19 03:13:06 +0300

    Make initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "ru.ileasile.kotlin"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
